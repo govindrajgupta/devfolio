b0aedf2518ba5e3e1606993c21771b21f9b18fb9
Govind Raj Gupta <gg7078417058@gmail.com>
2025-02-04 17:30:50 +0530

    lst

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/" sizes="any" />
+        <link rel="icon" href="/logo.svg" sizes="any" />
       </head>
       <body className={`${poppins.className}`}>
         {children}
